876fff61237b7c7c57b0a8b5ff3a809551e2798e
Logan Shahan <loganshahan88@gmail.com>
2019-02-03 14:27:11 -0500

    added body

---
+++ train.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyD66RTtjLRi92-vL8catscLBaJojwsDr3g",
+    authDomain: "train-schedule-33a2b.firebaseapp.com",
+    databaseURL: "https://train-schedule-33a2b.firebaseio.com",
+    projectId: "train-schedule-33a2b",
+    storageBucket: "train-schedule-33a2b.appspot.com",
+    messagingSenderId: "908917777262"
+  };
+  firebase.initializeApp(config);
+  
