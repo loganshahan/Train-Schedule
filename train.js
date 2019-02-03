1e320693230c93b1ca0f92c2e13b63b70296e9ab
Logan Shahan <loganshahan88@gmail.com>
2019-02-03 14:38:42 -0500

    added script

--- train.js
+++ train.js
@@ -7,4 +7,40 @@ var config = {
     messagingSenderId: "908917777262"
   };
   firebase.initializeApp(config);
-  
+  var database = firebase.database();
+
+  $("#add-train-btn").on("click", function(){
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainFirst = moment( $("#first-train-input").val().trim(),"HH:mm").format("X");
+    var trainFreq = $("frequency-input").val().trim();
+
+    var train = {
+        name : trainName,
+        destination: trainDest,
+        start: trainFirst,
+        frequency: trainFreq,
+    }
+    database.ref().push(train);
+})
+
+database.ref().on("child_added", function(snapshot) {
+    
+    
+    var trainName = snapshot.val().name
+    var trainDest = snapshot.val().role
+    var trainFirst = snapshot.val().start
+    var trainFreq = snapshot.val().rate
+    var date = moment.unix(empStart).format('MM/DD/YYYY');
+    var month = moment().diff(moment(empStart, "X"), "months");
+    var bill = month * empRate;
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFirst),
+        $("<td>").text(trainFreq),
+        $("<td>").text(minAway),
+      );
+      $("#employee-table > tbody").append(newRow);
+});
